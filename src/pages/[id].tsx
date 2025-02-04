d21f4d990ad00700ccdc1299e1d8455445a05d64
tetsuya2223 <yeah13261632@gmail.com>
2025-02-04 16:10:16 +0900

    fix:コードの修正

--- src/pages/[id].tsx
+++ src/pages/[id].tsx
@@ -9,41 +9,37 @@ const TodoDetails = () => {
   const [todo, setTodo] = useState<Todo | "empty" | null>(null);
 
   useEffect(() => {
-    if (!router.isReady) return; //クエリが取得できていない場合にuseEffectが実行しないように制御。
+    if (!router.isReady) return;
 
     const savedTodos = localStorage.getItem("todoArray");
+    if (!savedTodos) return;
 
-    if (savedTodos) {
-      const todos: Todo[] = JSON.parse(savedTodos);
+    const todos: Todo[] = JSON.parse(savedTodos);
 
-      if (todos.length === 0) {
-        setTodo("empty"); /// todosに値が入っていない場合、todoを"empty"に。
-      } else {
-        const findTodo = todos.find((todo) => todo.id === id);
-        setTodo(findTodo ?? "empty"); // todosに値が入っている場合、idが一致するtodoを探してset。
-      }
-    } else {
-      setTodo("empty"); //idが一致するものがない場合、todoを"empty"に。
+    if (todos.length === 0) {
+      setTodo("empty");
+      return;
     }
-  }, [id, router.isReady]); //isReadyがtrueになった時にuseEffectが実行されるようにする。
 
-  if (todo === null) return <div>通信中...</div>; //todo取得前の処理
-  if (todo === "empty") return <p>タスクが存在しません</p>; // 空データ処理
+    const findTodo = todos.find((todo) => todo.id === id);
 
+    setTodo(findTodo ?? "empty");
+  }, [id, router.isReady]);
+
+  if (todo === null) return <div>通信中...</div>;
+  if (todo === "empty") return <p>タスクが存在しません</p>;
   return (
     <div>
-      <>
-        <h1>TODO詳細</h1>
-        <p>
-          <strong>タスク:</strong> {todo.text}
-        </p>
-        <p>
-          <strong>締め切り日:</strong> {todo.dueDate || "なし"}
-        </p>
-        <p>
-          <strong>状態:</strong> {todo.isCompleted ? "完了済み" : "未完了"}
-        </p>
-      </>
+      <h1>TODO詳細</h1>
+      <p>
+        <strong>タスク:</strong> {todo.text}
+      </p>
+      <p>
+        <strong>締め切り日:</strong> {todo.dueDate || "なし"}
+      </p>
+      <p>
+        <strong>状態:</strong> {todo.isCompleted ? "完了済み" : "未完了"}
+      </p>
     </div>
   );
 };
