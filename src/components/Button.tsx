c3c82d74527b84628e502078ccb650415b4c090a
tetsuya2223 <yeah13261632@gmail.com>
2025-02-22 16:21:53 +0900

    feat:Buttonコンポーネントを作成。

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,1 +1,8 @@
-// ボタンコンポーネントファイルを作成
+type Props = {
+  type: "primary" | "secondary" | "tertiary";
+  text: string;
+};
+
+export const Button: React.FC<Props> = ({ type, text }) => {
+  return <button className={`button-${type}`}>{text}</button>;
+};
