a5d01cf95ab0339b985a664d3886e1f70624f953
tetsuya2223 <yeah13261632@gmail.com>
2025-02-22 16:44:08 +0900

    feat:Buttonコンポーネントで受け取るpropsを追加。

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,14 +1,25 @@
 import buttonStyles from "../styles/Home.module.css";
 
 type Props = {
-  type: "primary" | "secondary" | "tertiary";
+  buttonType: "button" | "submit" | "reset";
+  variant?: "primary" | "secondary" | "tertiary"; //?: 使用しなくても良い
   text: string;
+  onClick?: () => void;
 };
 
-export const Button: React.FC<Props> = ({ type, text }) => {
+export const Button: React.FC<Props> = ({
+  buttonType,
+  variant = "tertiary", // 指定しなければ"tertiary"
+  text,
+  onClick,
+}) => {
   return (
     <button
-      className={`${buttonStyles.button} ${buttonStyles[`button-${type}`]}`}
+      type={buttonType}
+      className={`${buttonStyles.button} ${
+        buttonStyles[`button-${buttonType}`]
+      }`}
+      onClick={onClick}
     >
       {text}
     </button>
