392cde82cfbb003ad4710bf3f40d7b06d884c75d
tetsuya2223 <yeah13261632@gmail.com>
2025-02-27 21:43:53 +0900

    feat:アンマウント時のアニメーションを実装

--- src/components/toast/index.tsx
+++ src/components/toast/index.tsx
@@ -1,14 +1,32 @@
 import { useToastContext } from "./provider";
+import { useFade } from "../../hooks/useFade";
 import toastStyles from "./style.module.css";
+import { useEffect } from "react";
 
 export const Toast: React.FC = () => {
-  const { isToastOpen, toastType } = useToastContext();
+  const { isToastOpen, toastType, closeToast } = useToastContext();
+  const { display, isFadingOut } = useFade(isToastOpen, 2000);
 
-  if (!isToastOpen) return null;
+  useEffect(() => {
+    if (!isToastOpen) return;
+
+    const timerId = window.setTimeout(() => {
+      closeToast();
+    }, 3000);
+
+    return () => clearTimeout(timerId);
+  }, [isToastOpen, closeToast]);
+
+  if (!display) return null;
 
   return (
     <div className={toastStyles.toastContainer}>
-      <div className={`${toastStyles.toast} ${toastStyles[toastType]} `}>
+      <div
+        className={`${toastStyles.toast} ${toastStyles[toastType]}
+        ${display ? toastStyles.show : ""} 
+        ${isFadingOut ? toastStyles.hidden : ""} 
+        `}
+      >
         {toastType === "success" ? "成功しました！" : "エラーが発生しました"}
       </div>
     </div>
--- src/components/toast/provider.tsx
+++ src/components/toast/provider.tsx
@@ -5,23 +5,17 @@ import { ToastContext } from "./context";
 export const ToastProvider = (props: PropsWithChildren) => {
   const { children } = props;
 
-  const [isToastOpen, setToastOpen] = useState(false);
+  const [isToastOpen, setIsToastOpen] = useState(false);
   const [toastType, setToastType] = useState<"success" | "error">("success");
 
   const showToast = (type: "success" | "error") => {
     setToastType(type);
-    setToastOpen(true);
+    setIsToastOpen(true);
   };
 
-  useEffect(() => {
-    if (!isToastOpen) return;
-
-    const timer = setTimeout(() => {
-      setToastOpen(false);
-    }, 3000);
-
-    return () => clearTimeout(timer);
-  }, [isToastOpen]);
+  const closeToast = () => {
+    setIsToastOpen(false);
+  };
 
   return (
     <ToastContext.Provider
@@ -29,6 +23,7 @@ export const ToastProvider = (props: PropsWithChildren) => {
         isToastOpen,
         toastType,
         showToast,
+        closeToast,
       }}
     >
       {children}
