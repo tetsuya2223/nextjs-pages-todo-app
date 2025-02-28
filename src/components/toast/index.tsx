1b1cf5a24c70ecc2245ad005ff0c6cf67f0c9d17
kimuniiii <kk.baseball.0824@gmail.com>
2025-02-28 11:22:42 +0900

    docs: TODOコメントを追加する

--- src/components/toast/index.tsx
+++ src/components/toast/index.tsx
@@ -9,6 +9,7 @@ export const Toast: React.FC = () => {
   return (
     <div className={toastStyles.toastContainer}>
       <div className={`${toastStyles.toast} ${toastStyles[toastType]}`}>
+        {/** TODO: Toastの中身のテキストを関数経由で変更できるようにする */}
         {toastType === "success" ? "成功しました！" : "エラーが発生しました"}
       </div>
     </div>
