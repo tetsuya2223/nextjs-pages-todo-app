1f9f3a3a774c0e117b18f6e64d80182b084e5f77
yeah2223 <yeah2223@gmail.com>
2025-03-04 13:22:09 +0900

    fix:eslint.config.mjsの修正

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import storybook from "eslint-plugin-storybook";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
